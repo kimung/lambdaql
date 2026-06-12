b7f3dd988d959da97bcbc8d95d76d2d265744d27
Kim Ung <3025641+kimung@users.noreply.github.com>
2026-06-12 10:17:33 +0200

    feat(data): introduce naming strategy for column mapping

---
+++ packages/data/src/naming.ts
@@ -0,0 +1,6 @@
+export type NamingStrategy = (property: string) => string
+
+export const identityNaming: NamingStrategy = (p) => p
+
+export const snakeCaseNaming: NamingStrategy = (p) =>
+  p.replace(/([A-Z])/g, '_$1').toLowerCase()
